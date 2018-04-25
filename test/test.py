0eb5c3023ab8692b91ed82e923d089fbad07c13f
Thomas Kwak <teekwak@gmail.com>
2018-04-24 23:55:47 -0700

    Add simple test source code

---
+++ test/test.py
@@ -0,0 +1,10 @@
+class Something:
+
+    def __init__(self):
+        pass
+
+    def do_something(self):
+        print("class asdf")
+
+    def do_something_else(self):
+        self.do_something()
